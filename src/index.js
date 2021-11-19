d48c4a83fb8c0cb892852de9781b5e5fe019504b
Olatunji Omisakin <msknolatunji@gmail.com>
2021-11-19 16:51:25 +0100

    work with map array method 3

--- src/index.js
+++ src/index.js
@@ -5,14 +5,23 @@ import './index.css'
 // setup var
 const books = [
   {
+    id: 1,
     img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
     title: 'I love you to the moon and back.',
     author: 'Amelia Hepworth',
   },
   {
+    id: 2,
     img: 'https://images-na.ssl-images-amazon.com/images/I/71hqSRFg5YL._AC_UL200_SR200,200_.jpg',
     title: 'Book of questions: Revised and Updated.',
     author: 'Gregory Stock',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81tqQak257L._AC_UL200_SR200,200_.jpg',
+    title:
+      'Make Your Bed: Little Things That Can Change Your Life... And Maybe The World.',
+    author: 'Admiral William H. McRaven',
   },
 ]
 
@@ -20,14 +29,14 @@ function Booklist() {
   return (
     <section className='booklist'>
       {books.map((book) => (
-        <Book img={book.img} title={book.title} author={book.author} />
+        <Book key={book.id} {...book} />
       ))}
     </section>
   )
 }
 
-const Book = (props) => {
-  const { img, title, author } = props
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props
   return (
     <article className='book'>
       <img src={img} alt='' />
