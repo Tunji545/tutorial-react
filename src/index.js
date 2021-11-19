2e8507b82755085fca6ea95da789b9189d286119
Olatunji Omisakin <msknolatunji@gmail.com>
2021-11-19 15:58:23 +0100

    work with map

--- src/index.js
+++ src/index.js
@@ -3,49 +3,34 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 // setup var
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
-  title: 'I love you to the moon and back.',
-  author: 'Amelia Hepworth',
-}
+const books = [
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title: 'I love you to the moon and back.',
+    author: 'Amelia Hepworth',
+  },
+  {
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71hqSRFg5YL._AC_UL200_SR200,200_.jpg',
+    title: 'Book of questions: Revised and Updated.',
+    author: 'Gregory Stock',
+  },
+]
 
-const secondBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/71hqSRFg5YL._AC_UL200_SR200,200_.jpg',
-  title: 'Book of questions: Revised and Updated.',
-  author: 'Gregory Stock',
-}
+const names = ['John', 'Peter', 'Bob', 'Susan', 'Anna']
+
+const newName = names.map((name) => <h1>{name}</h1>)
 
 function Booklist() {
-  return (
-    <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos corrupti
-          doloremque repudiandae cupiditate. Tempora accusantium officia omnis,
-          nobis quam sed.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  )
+  return <section className='booklist'>{newName}</section>
 }
 
-const Book = ({ img, title, author, children }) => {
-  // const { img, title, author } = props
+const Book = (props) => {
+  const { img, title, author } = props
   return (
     <article className='book'>
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
